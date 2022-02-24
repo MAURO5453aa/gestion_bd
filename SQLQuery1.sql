a4bca9fb5db0a9ac1a2f465edf976802eea88151
MAURO5453aa <86811014+MAURO5453aa@users.noreply.github.com>
2022-02-23 19:49:48 -0500

    punto 3

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,4 @@
+punto 1----------------------
 use facturacion
 begin tran 
 update productos set stock= precio *1.15
@@ -6,3 +7,11 @@ join categorias c on p.id_categoria = c.id_categoria
 where c.nombre <> 'Ferreteria'
 commit tran 
 
+
+punto 3-------------------------
+begin tran
+delete clientes 
+from clientes cl 
+left join facturas p on p.id_cliente = cl.id_cliente 
+where num_factura is null
+commit tran
