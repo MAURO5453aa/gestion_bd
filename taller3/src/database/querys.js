8af9a52977bb3b20f4c66d855ad5aee2b394bfe7
MAURO5453aa <86811014+MAURO5453aa@users.noreply.github.com>
2022-04-25 00:32:34 -0500

    Update querys.js

--- taller3/src/database/querys.js
+++ taller3/src/database/querys.js
@@ -6,4 +6,13 @@ export const querys = {
   deleteProduct: "DELETE FROM [facturacion].[dbo].[productos] WHERE id_producto= @id_producto",
   updateProductById:
     "UPDATE [facturacion].[dbo].[productos] SET Name = @nombre, precio = @precio, stock = @stock, id_categoria = @id_categoria WHERE id_producto = @id_producto",
+
+
+  getAllClientes: "SELECT * FROM  [facturacion].[dbo].[clientes]",
+  getClientesById: "SELECT * FROM clientes Where id_cliente = @id_cliente",
+  addNewClientes:
+    "INSERT INTO  [facturacion].[dbo].[clientes] (nombre, apellido, direccion, fecha_nacimiento,telefono,email) VALUES (@nombre,@apellido,@direccion,@fecha_nacimiento,@telefono,@email);",
+  deleteClientes: "DELETE FROM [facturacion].[dbo].[clientes] WHERE id_cliente= @id_cliente",
+  updateClietesById:
+    "UPDATE [facturacion].[dbo].[clientes] SET Name = nombre = @nombre, apellido = @apellido, direccion = @direccion, fecha_nacimiento = @fecha_nacimiento, telefono = @telefono, email = @email",
 };
