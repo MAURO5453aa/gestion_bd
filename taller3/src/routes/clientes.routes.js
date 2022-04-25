d9956a406025db3d41047fc9971dddb73d119a86
MAURO5453aa <86811014+MAURO5453aa@users.noreply.github.com>
2022-04-25 00:31:11 -0500

    Update clientes.routes.js

--- taller3/src/routes/clientes.routes.js
+++ taller3/src/routes/clientes.routes.js
@@ -1,1 +1,22 @@
+import { Router } from "express";
+import {
+  getClientes,
+  createNewCliente,
+  getClientesById,
+  deleteClientes,
+  updateClietesById,
+} from "../controllers/cliente.coller";
 
+const router = Router();
+
+router.get("/Cliente", getClientes);
+
+router.post("/Cliente", createNewCliente);
+
+router.get("/Cliente/:id", getClientesById);
+
+router.delete("/Cliente/:id", deleteClientes);
+
+router.put("/Cliente/:id", updateClietesById);
+
+export default router;
